f8c1dbec2e2bb84913675c28cf8e158d15ecd4ad
Pratham <ppratham812@gmail.com>
2024-03-24 08:47:08 +0530

    added request validation for auth

--- AuthService/src/routes/v1/index.js
+++ AuthService/src/routes/v1/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const UserController = require('../../controllers/user-controller');
-
+const {AuthRequestValidator} = require('../../middlewares/index');
 const router = express.Router();
 
-router.post('/signup',UserController.createUser);
-router.post('/signin',UserController.signIn);
+router.post('/signup',
+    AuthRequestValidator.validateUserAuth,
+    UserController.createUser
+);
+router.post('/signin',
+    AuthRequestValidator.validateUserAuth,
+    UserController.signIn
+);
 
 module.exports = router;
