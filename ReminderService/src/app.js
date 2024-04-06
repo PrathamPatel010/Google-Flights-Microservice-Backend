17ea73801838d4a505509929b7b5dcce634edd83
Pratham <ppratham812@gmail.com>
2024-04-06 14:56:24 +0530

    initialized reminder service with basic app setup

---
+++ ReminderService/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const {PORT} = require('./config/serverConfig');
+
+const app = express();
+const setupAndStartServer = () => {
+    app.use(express.json());
+    app.use(express.urlencoded({extended:true}));
+
+
+    app.listen(PORT,async()=>{
+        console.log(`Reminder Service is listening on port ${PORT}`);
+    });
+}
+
+setupAndStartServer();
