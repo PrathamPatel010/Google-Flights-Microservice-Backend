9919c9b67a240127c7a8d253a49d4294182c25ea
Pratham Patel <ppratham812@gmail.com>
2024-07-18 18:48:44 +0530

    feat:rate limiting feature developed

--- API_Gateway/src/app.js
+++ API_Gateway/src/app.js
@@ -2,11 +2,22 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const {createProxyMiddleware} = require('http-proxy-middleware');
+const rateLimit = require('express-rate-limit');
 const PORT = process.env.PORT;
 
 const app = express();
-app.use(morgan('combined'));
 
+// rate limiter
+// here we are allowing maximum 5 requests per minute coming from same IP
+const limiter = rateLimit({
+    windowMs: 1*60*1000,
+    max: 5
+});
+
+app.use(morgan('combined'));    // logger 
+app.use(limiter);
+
+// request redirection to appropriate service
 app.use('/flightservice',createProxyMiddleware({target:'http://localhost:3001/',changeOrigin:true}));
 app.use('/authservice',createProxyMiddleware({target:'http://localhost:3002/',changeOrigin:true}));
 app.use('/bookingservice',createProxyMiddleware({target:'http://localhost:3003/',changeOrigin:true}));
@@ -15,7 +26,6 @@ app.use('/reminderservice',createProxyMiddleware({target:'http://localhost:3004/
 app.get('/',(req,res)=>{
     return res.json({message:'API Gateway is up & running'});
 });
-
 app.listen(PORT,()=>{
     console.log(`API Gateway is listening on PORT ${PORT}`);
 });
